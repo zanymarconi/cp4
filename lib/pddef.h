da2ba9acab25ea0eefda5dcfa99350e9114bc205
zanymarconi <prakhardhama@gmail.com>
2025-08-18 09:14:00 +0530

    Sync

---
+++ lib/pddef.h
@@ -0,0 +1,16 @@
+#ifndef PDDEF_H
+#define PDDEF_H
+
+#include <limits>
+
+// Common data type definitions
+typedef long long ll; 
+
+// Shortcuts for common constants
+const int _INF = std::numeric_limits<int>::max();           // is 2^31 - 1 for 32-bits wide
+const int _MINF = std::numeric_limits<int>::min();          // is -2^31
+const ll _LLINF = std::numeric_limits<ll>::max();           // is 2^63 - 1 for 64-bits wide∂
+const ll _LLMINF = std::numeric_limits<ll>::min();          // is -2^63
+const double _EPS = std::numeric_limits<double>::epsilon(); // is 2^52 which is approx 2.22e-16
+
+#endif

5353b04bd0187d558fd2b4fd84bab0b6c07546e9
zanymarconi <prakhardhama@gmail.com>
2025-08-21 19:22:54 +0530

    create "prakhar" permutations

--- ch1_introduction/next_permutation.cpp
+++ ch1_introduction/next_permutation.cpp
@@ -1,11 +1,27 @@
 #include <pddef.h>
 using namespace std;
 
-void create_permutation(const string &s) {
-    string str = s;
-    sort(str.begin(), str.end()); // Sort the string to start with the smallest permutation
-    
+void create_permutation(const string &s) { 
     cout << "Permutations of \"" << s << "\":" << endl;
+
+    // Store in vector characters
+    vector<char> letters(s.begin(), s.end());
+    
+    // Sort the characters to ensure we start with the lexicographically smallest permutation
+    sort(letters.begin(), letters.end());
+
+    int count = 0; // To count the number of permutations
+    do {
+        // Print the current permutation
+        for (char c : letters) {
+            cout << c;
+        }
+        cout << endl;
+        count++;
+    } while (next_permutation(letters.begin(), letters.end())); // Generate the next permutation
+
+    // 7!/(2!*2!) = 1260 permutations for "prakhar"
+    cout << "Total permutations: " << count << endl;
 }
 
 int main() {

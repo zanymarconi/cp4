e52f9044d1208c365b83b7cc631bd25ed81164e9
zanymarconi <prakhardhama@gmail.com>
2025-09-03 09:32:09 +0530

    correct epsilon comment

--- lib/pddef.h
+++ lib/pddef.h
@@ -13,7 +13,7 @@ const int _INF = std::numeric_limits<int>::max();           // is 2^31 - 1 for 3
 const int _MINF = std::numeric_limits<int>::min();          // is -2^31
 const ll _LLINF = std::numeric_limits<ll>::max();           // is 2^63 - 1 for 64-bits wide∂
 const ll _LLMINF = std::numeric_limits<ll>::min();          // is -2^63
-const double _EPS = std::numeric_limits<double>::epsilon(); // is 2^52 which is approx 2.22e-16
+const double _EPS = std::numeric_limits<double>::epsilon(); // is 2^-52 which is approx 2.22e-16
 
 // Constants for mathematical operations
 const double _PI = M_PI;                                    // Pi constant
